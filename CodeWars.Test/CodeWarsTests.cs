7d204d101e8893f1d50341be35631bcdbf0b1625
Timur <wargerun@gmail.com>
2019-09-01 17:11:55 +0300

    next task

--- CodeWars.Test/CodeWarsTests.cs
+++ CodeWars.Test/CodeWarsTests.cs
@@ -69,5 +69,22 @@ namespace CodeWars.Test
 
             void Testing(string actual, string expected) => Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public static void Task_6_Test()
+        {
+            Console.WriteLine("Basic Tests SumDigPow");
+            Testing(1, 10, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            Testing(1, 100, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 89 });
+            Testing(10, 100, new long[] { 89 });
+            Testing(90, 100, new long[] { });
+            Testing(90, 150, new long[] { 135 });
+            Testing(50, 150, new long[] { 89, 135 });
+            Testing(10, 150, new long[] { 89, 135 });
+
+            void Testing(long a, long b, long[] res) => Assert.AreEqual(Array2String(res),Array2String(SumDigPower.SumDigPow(a, b)));
+
+            string Array2String(long[] list) => "[" + string.Join(", ", list) + "]";
+        }
     }
 }
---
+++ CodeWars.Test/Tasks/SumDigPower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.Test.Tasks
+{
+    public static class SumDigPower
+    {
+        public static long[] SumDigPow(long a, long b)
+        {
+            List<long> res = new List<long>();
+            for (long i = a; i <= b; i++)
+            {
+                int[] val = i.ToString().Select(x => int.Parse(x.ToString())).ToArray();
+                double sum = 0;
+
+                for (int j = 0; j < val.Length;)
+                {
+                    sum += Math.Pow(val[j], ++j);
+                }
+
+
+                if (sum == i)
+                    res.Add((long)sum);
+            }
+
+            return res.ToArray();
+        }
+    }
+}

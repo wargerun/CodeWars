5eff5d9bac74f3d08d2ff5d55142b961192c66e0
Timur <wargerun@gmail.com>
2020-02-09 19:11:33 +0300

    new diamond kata

--- CodeWars.Test/CodeWarsTests.cs
+++ CodeWars.Test/CodeWarsTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
-using System.Linq;
 using CodeWars.Test.Tasks;
+using System.Text;
 
 namespace CodeWars.Test
 {
@@ -153,5 +153,49 @@ namespace CodeWars.Test
             Assert.AreEqual("Max, John and Mark like this", PeopleLikes.Likes(new string[] { "Max", "John", "Mark" }));
             Assert.AreEqual("Alex, Jacob and 2 others like this", PeopleLikes.Likes(new string[] { "Alex", "Jacob", "Mark", "Max" }));
         }
+
+        #region Task_10_tests
+
+        [Test]
+        public void TestNull()
+        {
+            Assert.IsNull(Diamond.print(0));
+            Assert.IsNull(Diamond.print(-2));
+            Assert.IsNull(Diamond.print(2));
+        }
+
+        [Test]
+        public void TestDiamond1()
+        {
+            StringBuilder expected = new StringBuilder();
+            expected.Append("*\n");
+            Assert.AreEqual(expected.ToString(), Diamond.print(1));
+        }
+
+        [Test]
+        public void TestDiamond3()
+        {
+            StringBuilder expected = new StringBuilder();
+            expected.Append(" *\n");
+            expected.Append("***\n");
+            expected.Append(" *\n");
+
+            Assert.AreEqual(expected.ToString(), Diamond.print(3));
+        }
+
+        [Test]
+        public void TestDiamond5()
+        {
+            StringBuilder expected = new StringBuilder();
+            expected.Append("  *\n");
+            expected.Append(" ***\n");
+            expected.Append("*****\n");
+            expected.Append(" ***\n");
+            expected.Append("  *\n");
+
+            Assert.AreEqual(expected.ToString(), Diamond.print(5));
+        } 
+
+        #endregion
     }
 }
---
+++ CodeWars.Test/Tasks/Diamond.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace CodeWars.Test.Tasks
+{
+    /// <summary>
+    /// Jamie is a programmer, and James' girlfriend. 
+    /// She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+    ///
+    /// Task
+    ///    You need to return a string that looks like a diamond shape when printed on the screen, 
+    ///    using asterisk (*) characters.Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+    ///    
+    /// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+    /// 
+    /// Examples 
+    /// 
+    /// A size 3 diamond:
+    ///
+    ///  *
+    /// ***
+    ///  *
+    /// ...which would appear as a string of " *\n***\n *\n"
+    ///
+    /// A size 5 diamond:
+    ///
+    ///  *
+    /// ***
+    ///*****
+    /// ***
+    ///  * 
+    /// ...that is: " *\n ***\n*****\n ***\n *\n"
+    /// </summary>
+    internal class Diamond
+    {
+        public static string print(int n)
+        {
+            if (n % 2 == 0 || n < 0)
+            {
+                return null;
+            }
+
+            int middle = n / 2;
+            StringBuilder sb = new StringBuilder();
+            for (int index = 0; index < n; index++)
+            {
+                sb.Append(' ', Math.Abs(middle - index));
+                sb.Append('*', n - Math.Abs(middle - index) * 2);
+                sb.Append("\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+}

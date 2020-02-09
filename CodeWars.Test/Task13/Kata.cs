5265abd5219796a3ef713cd104dbbfc01dfc1666
Timur <wargerun@gmail.com>
2020-02-09 22:55:31 +0300

    new kata "Unique In Order"

---
+++ CodeWars.Test/Task13/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CodeWars.Test.Task13
+{
+    public class Kata
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            T current = default;
+            foreach (T x1 in iterable)
+            {
+                if (!x1.Equals(current))
+                {
+                    current = x1;
+                    yield return x1;
+                }
+            }
+        }
+    }
+}
---
+++ CodeWars.Test/Task13/SolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.Test.Task13
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.AreEqual("", Kata.UniqueInOrder(""));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("ABCDAB", Kata.UniqueInOrder("AAAABBBCCDAABBB"));
+        }
+    }
+}

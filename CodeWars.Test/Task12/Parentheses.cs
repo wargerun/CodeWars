0ee609280e10e1ae426a1afc75d8c2f6a4398af3
Timur <wargerun@gmail.com>
2020-02-09 21:14:20 +0300

    feat: new kata - valid parentheses

---
+++ CodeWars.Test/Task12/Parentheses.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace CodeWars.Test.Task12
+{
+    public class Parentheses
+    {
+        public static bool ValidParentheses(string input)
+        {
+            Hashtable map = new Hashtable(){
+                {'{','}'},
+                {'(',')'},
+                {'[',']'}
+            };
+
+            Stack stack = new Stack();
+
+            foreach (char c in input)
+            {
+                if (map.ContainsKey(c))
+                {
+                    stack.Push(c);
+                }
+                else if (map.ContainsValue(c))
+                {
+                    if (stack.Count > 0 && (char)map[(char)stack.Peek()] == c)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return (stack.Count == 0);
+        }
+    }
+}
---
+++ CodeWars.Test/Task12/SolutionTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.Test.Task12
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(true, Parentheses.ValidParentheses("()"));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses(")(((("));
+        }
+    }
+}

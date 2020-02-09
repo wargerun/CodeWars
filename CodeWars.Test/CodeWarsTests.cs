a7c8cdd2d6479a5e85f6d0e51ec684ca0ee322df
Timur <wargerun@gmail.com>
2020-02-09 18:26:11 +0300

    new likes map

--- CodeWars.Test/CodeWarsTests.cs
+++ CodeWars.Test/CodeWarsTests.cs
@@ -123,7 +123,7 @@ namespace CodeWars.Test
         }
 
         [Test]
-        public void TestCases()
+        public void Task_8_Tests()
         {
             Assert.AreEqual(true, IPValidation.is_valid_IP("0.0.0.0"));
             Assert.AreEqual(true, IPValidation.is_valid_IP("12.255.56.1"));
@@ -142,6 +142,16 @@ namespace CodeWars.Test
             Assert.AreEqual(false, IPValidation.is_valid_IP("12.34.56 .1"));
             Assert.AreEqual(false, IPValidation.is_valid_IP("12.34.56.-1"));
             Assert.AreEqual(false, IPValidation.is_valid_IP("123.045.067.089"));
+        }
+
+        [Test, Description("It should return correct text")]
+        public void Task_9_Tests()
+        {
+            Assert.AreEqual("no one likes this", PeopleLikes.Likes(new string[0]));
+            Assert.AreEqual("Peter likes this", PeopleLikes.Likes(new string[] { "Peter" }));
+            Assert.AreEqual("Jacob and Alex like this", PeopleLikes.Likes(new string[] { "Jacob", "Alex" }));
+            Assert.AreEqual("Max, John and Mark like this", PeopleLikes.Likes(new string[] { "Max", "John", "Mark" }));
+            Assert.AreEqual("Alex, Jacob and 2 others like this", PeopleLikes.Likes(new string[] { "Alex", "Jacob", "Mark", "Max" }));
         }
     }
 }
---
+++ CodeWars.Test/Tasks/PeopleLikes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CodeWars.Test.Tasks
+{
+    /// <summary>
+    /// You probably know the "like" system from Facebook and other pages. 
+    /// People can "like" blog posts, pictures or other items. 
+    /// We want to create the text that should be displayed next to such an item.
+    /// 
+    /// Implement a function likes :: [String] -> String, which must take in input array,
+    /// containing the names of people who like an item. 
+    /// It must return the display text as shown in the examples:
+    /// </summary>
+    class PeopleLikes
+    {
+        public static string Likes(string[] name)
+        {
+            switch (name.Length)
+            {
+                case 0:
+                    return  "no one likes this";
+                case 1:
+                    return $"{name[0]} likes this";
+                case 2:
+                    return $"{string.Join(" and ", name)} like this";
+                case 3:
+                    return  $"{name[0]}, {name[1]} and {name[2]} like this";                    
+                default:
+                    return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+            }
+        }
+    }
+}
